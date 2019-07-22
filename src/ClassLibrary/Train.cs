ce80285212d75c0e530b57cdaa205522d46c90d3
Eduardo Perez <eduardo.perez.2151@gmail.com>
2019-07-22 01:45:12 -0300

    docfx template

---
+++ src/ClassLibrary/Train.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    /// <summary>
+    /// This Class represent a basic Train
+    /// </summary>
+    public class Train
+    {
+        private bool _isStartedEngine;
+
+
+        /// <summary>
+        /// Start train`s engines
+        /// </summary>
+        /// <returns>
+        /// True if engines can be started, otherwise false
+        /// </returns>
+        public bool StartEngines()
+        {
+            if (_isStartedEngine)
+            {
+                Console.Write("The engines are already running");
+                return false;
+            }
+            _isStartedEngine = true;
+            Console.Write("Engines on");
+            return true;
+        }
+        
+        /// <summary>
+        /// Stop train`s engines
+        /// </summary>
+        /// <returns>
+        /// True if engines can be stopped, otherwise false
+        /// </returns>
+        public bool StopEngines()
+        {
+            if (_isStartedEngine)
+            {
+                _isStartedEngine = false;
+                Console.Write("Engines off");
+                return true;
+            }
+            Console.Write("The engines are already stopped");
+            return _isStartedEngine;
+        }
+        
+        
+    }
+}
--- src/ConsoleApplication/Program.cs
+++ src/ConsoleApplication/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ClassLibrary;
 
 namespace ConsoleApplication
 {
@@ -6,6 +7,7 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
+            var train = new Train();
             Console.WriteLine("Hello World!");
         }
     }
