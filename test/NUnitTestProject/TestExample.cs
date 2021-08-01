f7f28539046d81aabb3c7a35249c9faf66ad8ef7
fmachadopiriz <fmachadopiriz@outlook.com>
2021-08-01 18:45:09 -0300

    Added XML comments

--- test/NUnitTestProject/TestExample.cs
+++ test/NUnitTestProject/TestExample.cs
@@ -1,35 +1,55 @@
+//--------------------------------------------------------------------------------
+// <copyright file="TestExample.cs" company="Universidad Católica del Uruguay">
+//     Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//--------------------------------------------------------------------------------
+
 using ClassLibrary;
 using NUnit.Framework;
 
 namespace Tests
 {
+    /// <summary>
+    /// Prueba de la clase <see cref="Train"/>.
+    /// </summary>
     [TestFixture]
-    public class TestsExample
+    public class TestExample
     {
-
+        /// <summary>
+        /// El tren para probar.
+        /// </summary>
         private Train train;
 
+        /// <summary>
+        /// Crea un tren para probar.
+        /// </summary>
         [SetUp]
         public void Setup()
         {
             this.train = new Train();
         }
 
+        /// <summary>
+        /// Prueba que el tren arranque.
+        /// </summary>
         [Test]
         public void StartTrainTest()
         {
-            Assert.NotNull(train);
-            train.StartEngines();
-            Assert.True(train.IsEngineStarted);
+            Assert.NotNull(this.train);
+            this.train.StartEngines();
+            Assert.True(this.train.IsEngineStarted);
         }
 
+        /// <summary>
+        /// Prueba que el tren se detenga.
+        /// </summary>
         [Test]
         public void StopTrainTest()
         {
-            Assert.NotNull(train);
-            train.StartEngines();
-            train.StopEngines();
-            Assert.False(train.IsEngineStarted);
+            Assert.NotNull(this.train);
+            this.train.StartEngines();
+            this.train.StopEngines();
+            Assert.False(this.train.IsEngineStarted);
         }
     }
 }
