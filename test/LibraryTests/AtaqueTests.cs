e30f853df6ae8f4b274de3cccfbc687b877737bc
NicoLenzu <nicolas.lenzuen@correo.ucu.edu.uy>
2024-10-10 08:26:09 -0300

    Tests para la clase Ataque

--- test/LibraryTests/AtaqueTests.cs
+++ test/LibraryTests/AtaqueTests.cs
@@ -13,6 +13,34 @@ public class AtaqueTests
         Assert.Equals("Tacleada", ataque.Nombre);
         Assert.Equals(40, ataque.ValorAtaque);
     }
+    [Test]
+    public void TestAtaqueConstructor()
+    {
+        string nombre = "Llamarada";
+        int valorataque = 100;
+        Ataque ataque = new Ataque(nombre, valorataque);
+
+        Assert.Equals(nombre, ataque.Nombre);
+        Assert.Equals(valorataque, ataque.ValorAtaque);
+    }
+
+    [Test]
+    public void TestValorAtaque()
+    {
+        Ataque ataque = new Ataque("Corte", 50);
+        ataque.ValorAtaque = 75;
+            
+        Assert.Equals(75, ataque.ValorAtaque);
+    }
+
+    [Test]
+    public void TestNombre()
+    {
+        Ataque ataque = new Ataque("Corte", 50);
+        ataque.Nombre = "Cuchillada";
+            
+        Assert.Equals("Cuchillada", ataque.Nombre);
+    }
 
     
 }
