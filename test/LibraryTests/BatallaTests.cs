890541b6bc947de04d4da66d18070c4cb69aa3f6
mariamachado123 <mariae.machadom@correo.ucu.edu.uy>
2024-10-10 18:14:03 -0300

    Se agrego cambios dentro del jugador en BatallaTests.cs

--- test/LibraryTests/BatallaTests.cs
+++ test/LibraryTests/BatallaTests.cs
@@ -4,15 +4,13 @@ using ProyectoPokemon;
 
 namespace Tests
 {
-
     public class BatallaTests
     {
         [Test]
         public void BatallaTest()
         {
-            Efectividad efectividad = new Efectividad();
-            Jugador jugador1 = new Jugador();
-            Jugador jugador2 = new Jugador();
+            Jugador jugador1 = new Jugador("Diego");
+            Jugador jugador2 = new Jugador("Julia");
             Batalla batalla = new Batalla(jugador2, jugador1);
             var tipoElectrico = new Tipo("Electrico", new Ataque("Rayos", 5), new AtaqueEspecial("Rayos especiales", 10));
             var tipoAgua = new Tipo("Agua", new Ataque("Bolas de agua", 3),
