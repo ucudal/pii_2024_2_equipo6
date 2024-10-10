c1e2a9c9b3cbab20bbcf87d12c3a68c8cbd71080
NicoLenzu <nicolas.lenzuen@correo.ucu.edu.uy>
2024-10-10 09:12:08 -0300

    Agrego Tests Para la clase Tipo

---
+++ test/LibraryTests/TipoTests.cs
@@ -0,0 +1,49 @@
+using NUnit.Framework;
+using ProyectoPokemon;
+
+namespace Tests;
+
+public class TipoTests
+
+{
+    
+        [Test]
+        public void Test_TipoConstructor()
+        {
+            string nombre = "Fuego";
+            Ataque ataque = new Ataque("Llamarada", 100);
+            AtaqueEspecial ataqueEspecial = new AtaqueEspecial("Inferno", 120);
+
+            Tipo tipo = new Tipo(nombre, ataque, ataqueEspecial);
+
+            Assert.That(tipo.Nombre, Is.EqualTo(nombre));
+            Assert.That(tipo.Ataque, Is.EqualTo(ataque));
+            Assert.That(tipo.AtaqueEspecial, Is.EqualTo(ataqueEspecial));
+        }
+
+        [Test]
+        public void Test_AtaqueProperty()
+        {
+            Ataque ataqueInicial = new Ataque("Corte", 50);
+            Ataque nuevoAtaque = new Ataque("Cuchillada", 75);
+            Tipo tipo = new Tipo("Normal", ataqueInicial, new AtaqueEspecial("Poder Oculto", 60));
+
+            tipo.Ataque = nuevoAtaque;
+
+            Assert.That(tipo.Ataque, Is.EqualTo(nuevoAtaque));
+        }
+
+        [Test]
+        public void Test_AtaqueEspecialProperty()
+        {
+            AtaqueEspecial ataqueEspecialInicial = new AtaqueEspecial("Chispa", 60);
+            AtaqueEspecial nuevoAtaqueEspecial = new AtaqueEspecial("Rayo", 90);
+            Tipo tipo = new Tipo("Eléctrico", new Ataque("Impactrueno", 40), ataqueEspecialInicial);
+
+            tipo.AtaqueEspecial = nuevoAtaqueEspecial;
+
+            Assert.That(tipo.AtaqueEspecial, Is.EqualTo(nuevoAtaqueEspecial));
+        }
+    
+}
+
