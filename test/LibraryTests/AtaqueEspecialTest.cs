6f93e35ce63efb15c61d6a46df3c6763a674d7ad
mariamachado123 <mariae.machadom@correo.ucu.edu.uy>
2024-10-11 09:51:53 -0300

    Creación del AtaqueEspecialTest.cs

---
+++ test/LibraryTests/AtaqueEspecialTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using ProyectoPokemon;
+
+namespace Tests;
+
+public class AtaqueEspecialTest
+{
+    [Test]
+    public void TestAtaqueEspecial()
+    {
+        AtaqueEspecial ataqueEspecial = new AtaqueEspecial("Rompe Muros", 50);
+        Assert.That("Rompe Muros", Is.EqualTo(ataqueEspecial.Nombre));
+        Assert.That(50, Is.EqualTo(ataqueEspecial.ValorAtaqueEspecial));
+    }
+    
+    [Test]
+    public void TestAtaqueEspecialConstructor()
+    {
+        string nombre = "Rompe Cabezas";
+        int valorDelAtaque = 55;
+        AtaqueEspecial ataqueEspecial = new AtaqueEspecial(nombre, valorDelAtaque);
+    }
+
+    [Test]
+    public void ValorAtaqueEspecial()
+    {
+        AtaqueEspecial ataqueEspecial = new AtaqueEspecial("Patada de canguro", 50);
+        ataqueEspecial.ValorAtaqueEspecial = 78;
+        Assert.That(75, Is.EqualTo(ataqueEspecial.ValorAtaqueEspecial));
+    }
+
+    [Test]
+    public void NombreAtaqueEspecial()
+    {
+        AtaqueEspecial ataqueEspecial = new AtaqueEspecial("ValeBlanc", 65);
+        ataqueEspecial.Nombre = "Valita";
+        Assert.That("Valita", Is.EqualTo(ataqueEspecial.Nombre));
+    }
+}
