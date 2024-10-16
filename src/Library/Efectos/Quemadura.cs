8b673369b622834760e45d273e877ecd6d019fc9
NicoLenzu <nicolas.lenzuen@correo.ucu.edu.uy>
2024-10-16 16:36:51 -0300

    Creo la Interfaz de Efectos y los efectos bases sin implementar.

---
+++ src/Library/Efectos/Quemadura.cs
@@ -0,0 +1,14 @@
+﻿using ProyectoPokemon.Interfaces;
+
+namespace ProyectoPokemon.Efectos;
+
+public class Quemadura : IEfecto
+{
+    public string Nombre { get; set; }
+
+    private string nombre = "Quemadura";
+    public void AplicarEfecto()
+    {
+        
+    }
+}
