487c93726620fce0fcae71b39260cac4ceb6014d
mariamachado123 <mariae.machadom@correo.ucu.edu.uy>
2024-10-17 17:41:53 -0300

    Agregar lo necesaeio a AplicarEfecto

--- src/Library/Efectos/Quemadura.cs
+++ src/Library/Efectos/Quemadura.cs
@@ -7,8 +7,10 @@ public class Quemadura : IEfecto
     public string Nombre { get; set; }
 
     private string nombre = "Quemadura";
-    public void AplicarEfecto()
+    public void AplicarEfecto(Jugador receptor)
     {
-        
+        receptor.Pokemonelegido.EstaQuemado = true;
+        receptor.Pokemonelegido.TieneEstadoEspecial = true;
+        Console.WriteLine($"{receptor.Pokemonelegido.Nombre} ha sido quemado");
     }
 }
