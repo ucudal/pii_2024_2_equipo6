06d33bf266807bce15c1d84307d8cde9228a472a
mariamachado123 <mariae.machadom@correo.ucu.edu.uy>
2024-10-17 17:42:28 -0300

    Agregar lo necesario a AplicarEfecto

--- src/Library/Efectos/Somnolencia.cs
+++ src/Library/Efectos/Somnolencia.cs
@@ -4,11 +4,25 @@ namespace ProyectoPokemon.Efectos;
 
 public class Somnolencia : IEfecto
 {
+    private Random random = new Random();
     public string Nombre { get; set; }
-
     private string nombre = "Somnolencia";
-    public void AplicarEfecto()
+    public void AplicarEfecto(Jugador receptor)
     {
+        if (!receptor.Pokemonelegido.EstaDormido)
+        {
+            receptor.Pokemonelegido.EstaDormido = true;
+            receptor.Pokemonelegido.TieneEstadoEspecial = true;
+            receptor.Pokemonelegido.TurnosDormido = random.Next(1, 5);
+            
+            Console.WriteLine($"{receptor.Pokemonelegido.Nombre} ha sido dormido por su atacante");
+            
+        }
+        else
+        {
+            Console.WriteLine($"{receptor.Pokemonelegido.Nombre} ya esta dormido.");
+        }
+        
         
     }
 }
