db5e7e737cedd9b0f73f42e61b1d9d90a5a785b5
mariamachado123 <mariae.machadom@correo.ucu.edu.uy>
2024-10-18 14:54:26 -0300

    Ataques especiales e items para Batalla.cs

--- src/Library/Batalla.cs
+++ src/Library/Batalla.cs
@@ -13,6 +13,10 @@ public class Batalla
     private int contadorEfecto1 = 0;
     private int contadorEfecto2 = 0;
     private Random random = new Random();
+    private int contadorSuperPocion = 4;
+    private int contadorRevivir = 1;
+    private int contadorCuraTotal = 2;
+    
 
 
     public Batalla(Jugador jugador1, Jugador jugador2)
@@ -134,49 +138,203 @@ public class Batalla
     }
 
     public void ProcesarEfectosJugador(Jugador jugador, Jugador atacante)
-    
+
     {
         Efectividad efec = new Efectividad();
         double efectividad = efec.getEfectividad(atacante.Pokemonelegido.Tipo, jugador.Pokemonelegido.Tipo);
-        if (jugador.Pokemonelegido.TieneEstadoEspecial && jugador.Pokemonelegido.EstaDormido && jugador.Pokemonelegido.TurnosDormido>0)
+        if (jugador.Pokemonelegido.TieneEstadoEspecial && jugador.Pokemonelegido.EstaDormido &&
+            jugador.Pokemonelegido.TurnosDormido > 0)
         {
-            Console.WriteLine($"{jugador.Pokemonelegido.Nombre} esta dormido. Turnos restantes:{jugador.Pokemonelegido.TurnosDormido}");
-            jugador.Pokemonelegido.TurnosDormido--;
-            if (jugador.Pokemonelegido.TurnosDormido == 0)
+            if (random.NextDouble() * 100 > 49)
             {
-                jugador.Pokemonelegido.EstaDormido = false;
-                jugador.Pokemonelegido.TieneEstadoEspecial = false;
-                Console.WriteLine($"{jugador.Pokemonelegido.Nombre} despertó");
+                if (random.NextDouble() * 100 > 9)
+                {
+                    jugador.Pokemonelegido.TurnosDormido += 0.2;
+                    Console.WriteLine(
+                        $"{jugador.Pokemonelegido.Nombre} esta dormido. Turnos restantes:{jugador.Pokemonelegido.TurnosDormido}");
+                    jugador.Pokemonelegido.TurnosDormido--;
+                    if (jugador.Pokemonelegido.TurnosDormido == 0)
+                    {
+                        jugador.Pokemonelegido.EstaDormido = false;
+                        jugador.Pokemonelegido.TieneEstadoEspecial = false;
+                        Console.WriteLine($"{jugador.Pokemonelegido.Nombre} despertó");
+                    }
+                }
             }
+
         }
         else if (jugador.Pokemonelegido.EstaEnvenenado)
         {
-            double danoVeneno = (atacante.Pokemonelegido.AtaqueEspecial.ValorAtaque * efectividad) * 0.05;
-            jugador.Pokemonelegido.Hp -= danoVeneno;
-            Console.WriteLine($"{jugador.Pokemonelegido.Nombre} ha sido envenenado y perdió {danoVeneno} Hp");
+            if (random.NextDouble() * 100 > 49)
+            {
+                if (random.NextDouble() * 100 > 9)
+                {
+                    double danoVeneno = (atacante.Pokemonelegido.AtaqueEspecial.ValorAtaque * efectividad) * 0.25;
+                    jugador.Pokemonelegido.Hp -= danoVeneno;
+                    Console.WriteLine($"{jugador.Pokemonelegido.Nombre} ha sido envenenado y perdió {danoVeneno} Hp");
+                }
+                else
+                {
+                    double danoVeneno = (atacante.Pokemonelegido.AtaqueEspecial.ValorAtaque * efectividad) * 0.05;
+                    jugador.Pokemonelegido.Hp -= danoVeneno;
+                    Console.WriteLine($"{jugador.Pokemonelegido.Nombre} ha sido envenenado y perdió {danoVeneno} Hp");
+                }
+
+            }
         }
         else if (jugador.Pokemonelegido.EstaQuemado)
         {
-            double danoQuemadura = (atacante.Pokemonelegido.AtaqueEspecial.ValorAtaque * efectividad) * 0.10;
-            jugador.Pokemonelegido.Hp -= danoQuemadura;
-            Console.WriteLine($"{jugador.Pokemonelegido.Nombre} ha sido quemado y perdió {danoQuemadura} Hp");
+            if (random.NextDouble() * 100 > 49)
+            {
+                if (random.NextDouble() * 100 > 9)
+                {
+                    double danoQuemadura = (atacante.Pokemonelegido.AtaqueEspecial.ValorAtaque * efectividad) * 0.30;
+                    jugador.Pokemonelegido.Hp -= danoQuemadura;
+                    Console.WriteLine($"{jugador.Pokemonelegido.Nombre} ha sido quemado y perdió {danoQuemadura} Hp");
+                }
+                else
+                {
+                    double danoQuemadura = (atacante.Pokemonelegido.AtaqueEspecial.ValorAtaque * efectividad) * 0.10;
+                    jugador.Pokemonelegido.Hp -= danoQuemadura;
+                    Console.WriteLine($"{jugador.Pokemonelegido.Nombre} ha sido quemado y perdió {danoQuemadura} Hp");
+
+                }
+            }
         }
         else if (jugador.Pokemonelegido.EstaParalizado)
         {
-            if (random.NextDouble() * 100 > 25)
+            if (random.NextDouble() * 100 > 49)
             {
                 Console.WriteLine($"{jugador.Pokemonelegido.Nombre} esta paralizado y no puede atacar");
             }
             else
             {
-                Console.WriteLine($"{jugador.Pokemonelegido.Nombre} logra superar la paralisis. ");
+                Console.WriteLine($"{jugador.Pokemonelegido.Nombre} logra superar la paralisis. ")
             }
+
         }
     }
+
 
     public void Turno()
     {
         ProcesarEfectosJugador(jugador1,jugador2);
         ProcesarEfectosJugador(jugador2,jugador1);
     }
+
+   public void UtilizarItems(Jugador jugador)
+   {
+       Console.WriteLine($"DESEA USAR ALGUNO DE ESTOS ITEMS PARA{jugador.Pokemonelegido.Nombre}, su Hp es: {jugador.Pokemonelegido.Hp}:");       
+       Console.WriteLine("");                                                                                                                    
+       Console.WriteLine("1:SuperPocion:Recupera 70 puntos de HP");                                                                              
+       Console.WriteLine("");                                                                                                                    
+       Console.WriteLine("2.Revivir:Revive un Pokmeon con el 50% de su HP total");                                                               
+       Console.WriteLine("");                                                                                                                    
+       Console.WriteLine("3.CuraTotal:Cura a un pokemones");
+       Console.WriteLine(
+           "Si desea usar alguno de estos items, solo presione su numero correspondiente, de lo contrario, escriba NO:");
+       string respuesta = Console.ReadLine();
+       while (respuesta.ToUpper() != "NO")
+       {
+           if (respuesta == "1")
+           {
+               if (contadorSuperPocion > 0)
+               {
+                   jugador.Pokemonelegido.Hp += 70;
+                   if (jugador.Pokemonelegido.Hp > jugador.Pokemonelegido.VidaMaxima)
+                   {
+                       jugador.Pokemonelegido.Hp = jugador.Pokemonelegido.VidaMaxima;
+                   }
+                   contadorSuperPocion--;                                                                                                                            
+                   Console.WriteLine(                                                                                                                                
+                       $"Se recupero 70 puntos de la vida de {jugador.Pokemonelegido.Nombre}, podrá usar en adelante la SuperPocion: {contadorSuperPocion} veces");  
+               }
+               else
+               {
+                   Console.WriteLine(
+                       "No puede usar SuperPocion ya que agotó las posibilidades, porfavor intente de nuevo ");
+               }
+
+           }
+
+           if (respuesta == "2")
+           {
+               if (contadorRevivir > 0)
+               {
+                   jugador.Pokemonelegido.Hp = jugador.Pokemonelegido.Hp * 0.50;
+                   if (jugador.Pokemonelegido.Hp > jugador.Pokemonelegido.VidaMaxima)
+                   {
+                       jugador.Pokemonelegido.Hp = jugador.Pokemonelegido.VidaMaxima;
+                   }
+                   contadorRevivir--;                                                                                                                        
+                   Console.WriteLine(                                                                                                                        
+                       $"Aumento la vida de {jugador.Pokemonelegido.Nombre} un 50%, ahora tendrá {contadorRevivir} veces para usar el Revivir. ");           
+               }
+               else
+               {
+                   Console.WriteLine(
+                       "No puede usar Revivir ya que agotó las posibilidades, porfavor intente de nuevo ");
+
+               }
+           }
+
+           if (respuesta == "3")
+           {
+               if (contadorCuraTotal > 0)
+               {
+                   jugador.Pokemonelegido.EstaDormido = false;
+                   jugador.Pokemonelegido.EstaEnvenenado = false;
+                   jugador.Pokemonelegido.TurnosDormido = 0;
+                   jugador.Pokemonelegido.EstaParalizado = false;
+                   jugador.Pokemonelegido.EstaQuemado = false;
+                   contadorCuraTotal--;
+                   Console.WriteLine(
+                       $"Su pokemon se curo de: {jugador.Pokemonelegido.EfectoActual}, ahora tendra {contadorCuraTotal} veces para usar la Cura Total.");
+               }
+               else
+               {
+                   Console.WriteLine(
+                       "No puede usar Cura Total ya que agotó las posibilidades, porfavor intente de nuevo.");
+               }
+           }
+           else
+           {
+               Console.WriteLine("Caracter no valido, intente de nuevo.");
+           }
+
+           Console.WriteLine(
+               $"DESEA USAR ALGUNO DE ESTOS ITEMS PARA{jugador.Pokemonelegido.Nombre}, su Hp es: {jugador.Pokemonelegido.Hp}");
+           respuesta = Console.ReadLine();
+       }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   }                                                                                                                                         
+       
+
 }
