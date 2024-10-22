b4fb442f4050ff4e0e73f6f856259c2629fb63fa
mariamachado123 <mariae.machadom@correo.ucu.edu.uy>
2024-10-22 20:32:37 -0300

    Agregar comparacion de si recien lo quemaron o si ya lo estaba,para que no se vuelva repetitivo, en mensajes, comportamientos, etc.

--- src/Library/Efectos/Quemadura.cs
+++ src/Library/Efectos/Quemadura.cs
@@ -7,10 +7,20 @@ public class Quemadura : IEfecto
     public string Nombre { get; set; }
 
     private string nombre = "Quemadura";
+
     public void AplicarEfecto(Jugador receptor)
     {
-        receptor.Pokemonelegido.EstaQuemado = true;
-        receptor.Pokemonelegido.TieneEstadoEspecial = true;
-        Console.WriteLine($"{receptor.Pokemonelegido.Nombre} ha sido quemado");
+        if (!receptor.Pokemonelegido.EstaQuemado)
+        {
+            receptor.Pokemonelegido.EstaQuemado = true;
+            receptor.Pokemonelegido.TieneEstadoEspecial = true;
+            Console.WriteLine($"{receptor.Pokemonelegido.Nombre} ha sido quemado");
+
+        }
+        else
+        {
+            Console.WriteLine($"{receptor.Pokemonelegido.Nombre} ya esta quemado.");
+        }
+
     }
 }
