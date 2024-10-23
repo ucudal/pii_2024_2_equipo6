7cf5dd41262945110ef7ce4068a24065968c6799
andreagonzalezucu <andrea.gonzalezr@correo.ucu.edu.uy>
2024-10-23 09:40:37 -0300

    Correción AtaqueEspecialTest.cs

--- test/LibraryTests/AtaqueEspecialTest.cs
+++ test/LibraryTests/AtaqueEspecialTest.cs
@@ -1,5 +1,7 @@
 ﻿using NUnit.Framework;
 using ProyectoPokemon;
+using ProyectoPokemon.Efectos;
+using ProyectoPokemon.Interfaces;
 
 namespace Tests;
 
@@ -8,31 +10,38 @@ public class AtaqueEspecialTest
     [Test]
     public void TestAtaqueEspecial()
     {
-        AtaqueEspecial ataqueEspecial = new AtaqueEspecial("Rompe Muros", 50);
+        IEfecto paralisis = new Paralisis();
+        AtaqueEspecial ataqueEspecial = new AtaqueEspecial("Rompe Muros", 50, paralisis);
         Assert.That("Rompe Muros", Is.EqualTo(ataqueEspecial.Nombre));
-        Assert.That(50, Is.EqualTo(ataqueEspecial.ValorAtaqueEspecial));
+        Assert.That(50, Is.EqualTo(ataqueEspecial.ValorAtaque));
     }
     
     [Test]
     public void TestAtaqueEspecialConstructor()
     {
+        IEfecto somnolencia = new Somnolencia();
         string nombre = "Rompe Cabezas";
         int valorDelAtaque = 55;
-        AtaqueEspecial ataqueEspecial = new AtaqueEspecial(nombre, valorDelAtaque);
+        AtaqueEspecial ataqueEspecial = new AtaqueEspecial(nombre, valorDelAtaque, somnolencia);
+        Assert.That("Rompe Cabezas", Is.EqualTo(ataqueEspecial.Nombre));
+        Assert.That(valorDelAtaque, Is.EqualTo(ataqueEspecial.ValorAtaque));
+        Assert.That(somnolencia, Is.EqualTo(ataqueEspecial.Efecto));
     }
 
     [Test]
     public void ValorAtaqueEspecial()
     {
-        AtaqueEspecial ataqueEspecial = new AtaqueEspecial("Patada de canguro", 50);
-        ataqueEspecial.ValorAtaqueEspecial = 78;
-        Assert.That(75, Is.EqualTo(ataqueEspecial.ValorAtaqueEspecial));
+        IEfecto paralisis = new Paralisis();
+        AtaqueEspecial ataqueEspecial = new AtaqueEspecial("Patada de canguro", 50, paralisis);
+        ataqueEspecial.ValorAtaque = 78;
+        Assert.That(75, Is.EqualTo(ataqueEspecial.ValorAtaque));
     }
 
     [Test]
     public void NombreAtaqueEspecial()
     {
-        AtaqueEspecial ataqueEspecial = new AtaqueEspecial("ValeBlanc", 65);
+        IEfecto quemadura = new Quemadura();
+        AtaqueEspecial ataqueEspecial = new AtaqueEspecial("ValeBlanc", 65, quemadura);
         ataqueEspecial.Nombre = "Valita";
         Assert.That("Valita", Is.EqualTo(ataqueEspecial.Nombre));
     }
