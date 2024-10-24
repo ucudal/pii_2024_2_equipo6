18ff00a57ba7a92b33e4267c1979ff66db7ed305
mariamachado123 <mariae.machadom@correo.ucu.edu.uy>
2024-10-24 15:41:29 -0300

    Comentacion de ataque comun

--- src/Library/AtaqueComun.cs
+++ src/Library/AtaqueComun.cs
@@ -2,11 +2,11 @@ namespace ProyectoPokemon;
 
 public class AtaqueComun : IAtaque
 {
-    public string Nombre { get; set; }
+    public string Nombre { get; set; }//inicializamos el nombre, para cuando lo llamemos, tenga uno
     public int ValorAtaque { get; set; }
-    public AtaqueComun(string nombre, int valorataque)
+    public AtaqueComun(string nombre, int valorataque) //generamos un constructor al ataque comun, para poder ingresarle el nombre, y su valor de ataque
     {
-        this.Nombre = nombre;
+        this.Nombre = nombre; //en este lugar se guarda lo que se ingresa en el constructor
         this.ValorAtaque = valorataque;
     }
 }
