017e8000f21faf2d8200bf9648aa8114ef15af2b
mariamachado123 <mariae.machadom@correo.ucu.edu.uy>
2024-10-24 15:42:24 -0300

    comentar

--- src/Library/AtaqueEspecial.cs
+++ src/Library/AtaqueEspecial.cs
@@ -4,20 +4,20 @@ namespace ProyectoPokemon;
 
 public class AtaqueEspecial : IAtaque
 {
-    public string Nombre { get; set; }
+    public string Nombre { get; set; }//inicializamos el nombre, para cuando lo llamemos, tenga uno
     public int ValorAtaque { get; set; }
 
-    public IEfecto Efecto
+    public IEfecto Efecto //genera un efecto, que será parte del ataque especial
     {
-        get { return Efecto; }
+        get { return Efecto; } 
         set { Efecto = value; }
     }
     
     
 
-    public AtaqueEspecial(string nombre, int valorataqueespecial, IEfecto efecto)
+    public AtaqueEspecial(string nombre, int valorataqueespecial, IEfecto efecto)//generamos un constructor al ataque especial, para poder ingresarle el nombre,  su valor de ataque y su efecto
     {
-        this.Nombre = nombre + " (Especial)";
+        this.Nombre = nombre + " (Especial)"; //en este lugar se guarda lo que se ingresa en el constructor
         this.ValorAtaque = valorataqueespecial;
         this.Efecto = efecto;
     }
